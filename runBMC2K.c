497aaf1d2b9a033a36c1c4bbd6c7274b15ba3b75
Kyle Van Gorkom <kyle.vangorkom@gmail.com>
2019-01-10 19:36:55 -0700

    add command line options and bias and clipping behaviors

--- runBMC2K.c
+++ runBMC2K.c
@@ -1,6 +1,6 @@
 /*
 To compile:
-gcc -o build/runBMC2K runBMC2K.c -I/opt/Boston\ Micromachines/include -L/opt/Boston\ Micromachines/lib -Wl,-rpath-link,/opt/Boston\ Micromachines/lib -lBMC -lBMC_PCIeAPI -lncurses -lImageStreamIO -lpthread
+gcc -o build/runBMC2K runBMC2K.c -I/opt/Boston\ Micromachines/include -L/opt/Boston\ Micromachines/lib -Wl,-rpath-link,/opt/Boston\ Micromachines/lib -lBMC -lBMC_PCIeAPI -lncurses -lImageStreamIO -lpthread -lrt -lm
 */
 
 /* BMC */
@@ -16,6 +16,8 @@ gcc -o build/runBMC2K runBMC2K.c -I/opt/Boston\ Micromachines/include -L/opt/Bos
 #include <signal.h>
 #include <curses.h>
 #include <unistd.h>
+#include <math.h>
+#include <argp.h>
 
 typedef int bool_t;
 
@@ -32,7 +34,7 @@ void handle_signal(int signal)
 }
 
 // Initialize the shared memory image
-void initializeSharedMemory(char * serial, int nbAct)
+void initializeSharedMemory(char * serial, uint32_t nbAct)
 {
     long naxis; // number of axis
     uint8_t atype;     // data type
@@ -78,16 +80,77 @@ void initializeSharedMemory(char * serial, int nbAct)
     SMimage[0].md[0].cnt1++;
 }
 
-BMCRC sendCommand(DM hdm, uint32_t *map_lut, IMAGE * SMimage) {
+/* Remove DC bias in inputs to maximize actuator range */
+void bias_inputs(double * command, uint32_t ActCount)
+{
+    int idx;
+    double mean;
+    double cenval;
+
+    // calculate mean value
+    mean = 0;
+    for ( idx = 0 ; idx < ActCount ; idx++)
+    {
+        mean += command[idx];
+    }
+    mean /= ActCount;
+    printf("I found a mean of: %f\n", mean);
+
+    /* Remove mean from each actuator input
+    and add voltage bias to center of range.
+    */
+    cenval = sqrt(0.5);
+    for ( idx = 0 ; idx < ActCount ; idx++)
+    {
+        command[idx] += cenval - mean;
+    }
+    printf("Adding bias of: %f\n", cenval);
+}
+
+/* Convert any DM inputs to [0, 1] to avoid 
+exceeding safe DM operation. */
+void clip_to_limits(double * command, uint32_t ActCount)
+{
+    int idx;
+    // check each actuator and clip if needed
+    for ( idx = 0 ; idx < ActCount ; idx++)
+    {
+        if (command[idx] > 1.0)
+        {
+            printf("Actuator %d saturated!\n", idx + 1);
+            command[idx] = 1.0;
+        } else if (command[idx] < 0.0)
+        {
+            printf("Actuator %d saturated!\n", idx + 1);
+            command[idx] = 0.0;
+        }
+    }
+}
+
+BMCRC sendCommand(DM hdm, uint32_t *map_lut, IMAGE * SMimage, int nobias) {
     // Initialize variables
     double *command;
     int idx;
+    uint32_t ActCount;
     BMCRC rv;
 
     // Cast to array type ALPAO expects
-    command = (double*)calloc((int)hdm.ActCount, sizeof(double));
-    for (idx = 0; idx < (int)hdm.ActCount; idx++) {
+    ActCount = (uint32_t)hdm.ActCount;
+    command = (double*)calloc(ActCount, sizeof(double));
+    for (idx = 0; idx < ActCount; idx++) {
         command[idx] = SMimage[0].array.D[idx];
+    }
+
+    // Apply the bias
+    if (nobias != 1) {
+        bias_inputs(command, ActCount);
+    }
+
+    // Clip to limits
+    clip_to_limits(command, ActCount);
+
+    for (idx = 0; idx < ActCount; idx++) {
+        printf("Act %d: %f\n", idx, command[idx]);
     }
 
     // Send command
@@ -105,17 +168,18 @@ BMCRC sendCommand(DM hdm, uint32_t *map_lut, IMAGE * SMimage) {
 }
 
 // intialize DM and shared memory and enter DM command loop
-int controlLoop() {
+int controlLoop(char * serial_number, int nobias) {
 
     // Initialize variables
     DM hdm = {};
     BMCRC rv;
     int idx;
+    uint32_t ActCount;
     uint32_t *map_lut;
     IMAGE * SMimage;
 
     // Open driver
-    char serial_number[12] = "27BW027#081";
+    //char serial_number[12] = "27BW027#081";
     rv = BMCOpen(&hdm, serial_number);
     // Check for errors
     if(rv) {
@@ -123,19 +187,19 @@ int controlLoop() {
         printf("%s\n\n", BMCErrorString(rv));
         return rv;
     }
+    ActCount = (uint32_t)hdm.ActCount;
 
-    printf("Opened Device %d with %d actuators.\n", hdm.DevId, hdm.ActCount);
+    printf("Opened Device %d with %d actuators.\n", hdm.DevId, ActCount);
 
     // Load actuator map
     map_lut = (uint32_t *)malloc(sizeof(uint32_t)*MAX_DM_SIZE);
-    for(idx=0; idx<(int)hdm.ActCount; idx++) {
+    for(idx=0; idx<ActCount; idx++) {
         map_lut[idx] = 0;
     }
     rv = BMCLoadMap(&hdm, NULL, map_lut);
 
     // initialize shared memory image to 0s
-    initializeSharedMemory(serial_number, (int)hdm.ActCount);
-
+    initializeSharedMemory(serial_number, ActCount);
     // connect to shared memory image (SMimage)
     SMimage = (IMAGE*) malloc(sizeof(IMAGE));
     ImageStreamIO_read_sharedmem_image_toIMAGE(serial_number, &SMimage[0]);
@@ -145,7 +209,7 @@ int controlLoop() {
         printf("SM image naxis = %d\n", SMimage[0].md[0].naxis);
         return -1;
     }
-    if (SMimage[0].md[0].size[0] != hdm.ActCount) {
+    if (SMimage[0].md[0].size[0] != ActCount) {
         printf("SM image size (axis 1) = %d", SMimage[0].md[0].size[0]);
         return -1;
     }
@@ -153,7 +217,7 @@ int controlLoop() {
     // set DM to all-0 state to begin
     printf("BMC %s: initializing all actuators to 0.\n", serial_number);
     ImageStreamIO_semwait(&SMimage[0], 0);
-    rv  = sendCommand(hdm, map_lut, SMimage);
+    rv  = sendCommand(hdm, map_lut, SMimage, nobias);
     if (rv) {
         printf("Error %d sending command.\n", rv);
         return rv;
@@ -174,7 +238,7 @@ int controlLoop() {
         
         // Send Command to DM
         if (!stop) { // Skip DM on interrupt signal
-            rv = sendCommand(hdm, map_lut, SMimage);
+            rv = sendCommand(hdm, map_lut, SMimage, nobias);
             if (rv) {
                 printf("Error %d sending command.\n", rv);
                 return rv;
@@ -201,11 +265,79 @@ int controlLoop() {
 }
 
 
+/*
+Argument parsing
+*/
+
+/* Program documentation. */
+static char doc[] =
+  "runBMC2K-- enter the BMC2K DM command loop and wait for milk shared memory images to be posted at <serial>";
+
+/* A description of the arguments we accept. */
+static char args_doc[] = "serial";
+
+/* The options we understand. */
+static struct argp_option options[] = {
+  {"nobias",     'b', 0, 0,  "Disable automatically biasing the DM (enabled by default)" },
+  { 0 }
+};
+
+/* Used by main to communicate with parse_opt. */
+struct arguments
+{
+  char *args[1];                /* serial */
+  int nobias;
+};
+
+/* Parse a single option. */
+static error_t parse_opt (int key, char *arg, struct argp_state *state)
+{
+  /* Get the input argument from argp_parse, which we
+     know is a pointer to our arguments structure. */
+  struct arguments *arguments = state->input;
+
+  switch (key)
+    {
+    case 'b':
+      arguments->nobias = 1;
+      break;
+    case ARGP_KEY_ARG:
+      if (state->arg_num >= 1)
+        /* Too many arguments. */
+        argp_usage (state);
+
+      arguments->args[state->arg_num] = arg;
+
+      break;
+
+    case ARGP_KEY_END:
+      if (state->arg_num < 1)
+        /* Not enough arguments. */
+        argp_usage (state);
+      break;
+
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
+
+/* Our argp parser. */
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
+
 int main(int argc, char* argv[]) {
 
-    // add bias, etc. options here
+    struct arguments arguments;
 
-    BMCRC rv = controlLoop();
+    /* Default values. */
+    arguments.nobias = 0;
+
+    /* Parse our arguments; every option seen by parse_opt will
+     be reflected in arguments. */
+    argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
+    BMCRC rv = controlLoop(arguments.args[0], arguments.nobias);
     if (rv) {
         printf("Encountered error %d.\n", rv);
         return rv;
